c4d627479577180438f24d2de8e933046b96848c
Cole Parker <cole.parker@hotmail.ca>
2026-09-01 21:30:41 -0600

    Add custom format

---
+++ config/custom-formats.ts
@@ -0,0 +1,21 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: import('../sim/dex-formats').FormatList = [
+
+	// Emerald Nuzhacke
+	///////////////////////////////////////////////////////////////////
+
+	{
+		section: "Emerald Nuzhacke",
+	},
+	{
+		name: "[Gen 9] Emerald Nuzhacke VGC",
+		desc: "VGC rules (Team Preview, Species Clause, Item Clause, Level 50, Open Team Sheets, Bring 6 Pick 4), but every Pok&eacute;mon is allowed and any move/ability combination is legal on it.",
+		mod: 'gen9',
+		gameType: 'doubles',
+		bestOfDefault: true,
+		ruleset: ['Flat Rules', 'NatDex Mod', 'VGC Timer', 'Open Team Sheets', '!Obtainable Moves', '!Obtainable Abilities'],
+		unbanlist: ['Mythical', 'Restricted Legendary', 'Greninja-Bond'],
+	},
+];
